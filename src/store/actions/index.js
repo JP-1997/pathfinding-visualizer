4f7db98414ee23c59a89dd14d1b6ac28e70244c6
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2020-08-31 23:26:53 +0530

    added redux store

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,55 +1,55 @@
 export const selectAlgo = algoId => {
     return {
-      type: "SELECT_ALGO",
-      payload: algoId
+        type: "SELECT_ALGO",
+        payload: algoId
     };
-  };
-  
-  export const allowDiag = allowDiag => {
+};
+
+export const allowDiag = allowDiag => {
     return {
-      type: "ALLOW_DIAG",
-      payload: allowDiag
+        type: "ALLOW_DIAG",
+        payload: allowDiag
     };
-  };
-  
-  export const changeHeuristic = heuristic => {
+};
+
+export const changeHeuristic = heuristic => {
     return {
-      type: "CHANGE_HEURISTIC",
-      payload: heuristic
+        type: "CHANGE_HEURISTIC",
+        payload: heuristic
     };
-  };
-  
-  export const selectMaze = maze => {
+};
+
+export const selectMaze = maze => {
     return {
-      type: "SELECT_MAZE",
-      payload: maze
+        type: "SELECT_MAZE",
+        payload: maze
     };
-  };
-  
-  export const animateMaze = animMaze => {
+};
+
+export const animateMaze = animMaze => {
     return {
-      type: "ANIMATE_MAZE",
-      payload: animMaze
+        type: "ANIMATE_MAZE",
+        payload: animMaze
     };
-  };
-  
-  export const setAnimating = isAnim => {
+};
+
+export const setAnimating = isAnim => {
     return {
-      type: "SET_ANIMATING",
-      payload: isAnim
+        type: "SET_ANIMATING",
+        payload: isAnim
     };
-  };
-  
-  export const setVisited = visited => {
+};
+
+export const setVisited = visited => {
     return {
-      type: "SET_VISITED",
-      payload: visited
+        type: "SET_VISITED",
+        payload: visited
     };
-  };
-  
-  export const setShortest = shortest => {
+};
+
+export const setShortest = shortest => {
     return {
-      type: "SET_SHORTEST",
-      payload: shortest
+        type: "SET_SHORTEST",
+        payload: shortest
     };
-  };
+};
